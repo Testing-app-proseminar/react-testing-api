e0bee5c5774282142dbca578a4fbb614311d58e4
Monica Sanchez <monicaa.sanchezv@gmail.com>
2024-01-18 19:18:57 +0100

    Create Add card functionality

---
+++ src/pages/CardsPage.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import AddCard from "../components/AddCard";
+
+function CardsPage() {
+  const [cards, setCards] = useState([]);
+
+  const getAllCards = () => {
+
+    const storedToken = localStorage.getItem("authToken");
+
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/cards`, {headers: {Authorization: `Bearer ${storedToken}`}})
+      .then((response) => setCards(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllCards();
+  }, []);
+
+  return (
+    <div className="CardsPage">
+
+      <AddCard/>
+
+      {cards.map((card) => {
+        return (
+          <div className="CardsPageCard" key={card._id}>
+            <Link to={`/cards/${card._id}`}>
+              <h3>{card.title}</h3>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default CardsPage;
