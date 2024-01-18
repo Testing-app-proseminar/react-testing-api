4a77ce3eebdf3ace27e4297ef4673d8d0ee37626
Monica Sanchez <monicaa.sanchezv@gmail.com>
2024-01-18 19:20:06 +0100

    Create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+
+function Navbar() {
+
+    const {isLoggedIn, user, logOutUser } = useContext(AuthContext)
+
+    return (
+        <nav>
+        <div>
+            <Link to="/">
+                <button>Home</button>
+            </Link>
+
+            {isLoggedIn && (
+                <>
+                <Link to="/cards">
+                    <button>Study Cards</button>
+                </Link>
+                <button onClick={logOutUser}>Logout</button>
+                <span>{user && user.email}</span>
+                </>
+            )}
+
+            {!isLoggedIn && (
+                <>
+                <Link to="/signup">
+                    <button>Signup</button>
+                </Link>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
+                </>
+            )}
+
+        </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import AddCard from "../components/AddCard";
+
+function HomePage() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    );
+  }
+   
+  export default HomePage;
