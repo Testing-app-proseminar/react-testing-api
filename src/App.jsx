0ead26efb7c3fdd5fd50b8128f6505a956c4329b
Monica Sanchez <monicaa.sanchezv@gmail.com>
2024-01-18 19:20:26 +0100

    Add routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import "./App.css";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CardsPage from "./pages/CardsPage";
+import Navbar from "./components/Navbar";
+import IsPrivate from "./components/IsPrivate";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/cards" element={< CardsPage />} />
       </Routes>
     </>
   );
